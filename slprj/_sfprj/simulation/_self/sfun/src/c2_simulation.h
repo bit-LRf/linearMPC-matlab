7463fe58735c780a6e3a59a781499cf473f00d56
李睿峰 <LRf@MSI.com>
2023-06-05 13:32:51 +0800

    2023/6/5

---
+++ slprj/_sfprj/simulation/_self/sfun/src/c2_simulation.h
@@ -0,0 +1,501 @@
+#ifndef __c2_simulation_h__
+#define __c2_simulation_h__
+
+/* Forward Declarations */
+#ifndef typedef_c2_s7m1Hk28rRB73vIGeeW9J6G
+#define typedef_c2_s7m1Hk28rRB73vIGeeW9J6G
+
+typedef struct c2_tag_s7m1Hk28rRB73vIGeeW9J6G c2_s7m1Hk28rRB73vIGeeW9J6G;
+
+#endif                                 /* typedef_c2_s7m1Hk28rRB73vIGeeW9J6G */
+
+#ifndef typedef_c2_ros_TimeStruct_T
+#define typedef_c2_ros_TimeStruct_T
+
+typedef struct c2_tag_sa1bWj2vFQaBkpnMZWMpj5 c2_ros_TimeStruct_T;
+
+#endif                                 /* typedef_c2_ros_TimeStruct_T */
+
+#ifndef typedef_c2_smzGQHcQ1fZcSCW5rtLpn4F
+#define typedef_c2_smzGQHcQ1fZcSCW5rtLpn4F
+
+typedef struct c2_tag_smzGQHcQ1fZcSCW5rtLpn4F c2_smzGQHcQ1fZcSCW5rtLpn4F;
+
+#endif                                 /* typedef_c2_smzGQHcQ1fZcSCW5rtLpn4F */
+
+#ifndef typedef_c2_sdSMOHVTauEd1rUkHy24MYH
+#define typedef_c2_sdSMOHVTauEd1rUkHy24MYH
+
+typedef struct c2_tag_sdSMOHVTauEd1rUkHy24MYH c2_sdSMOHVTauEd1rUkHy24MYH;
+
+#endif                                 /* typedef_c2_sdSMOHVTauEd1rUkHy24MYH */
+
+#ifndef typedef_c2_s_bhoJb8DGvrqWQvPGAcJgFD
+#define typedef_c2_s_bhoJb8DGvrqWQvPGAcJgFD
+
+typedef struct c2_tag_bhoJb8DGvrqWQvPGAcJgFD c2_s_bhoJb8DGvrqWQvPGAcJgFD;
+
+#endif                                 /* typedef_c2_s_bhoJb8DGvrqWQvPGAcJgFD */
+
+#ifndef typedef_c2_cell_0
+#define typedef_c2_cell_0
+
+typedef struct c2_tag_0U5Qpi6GXqcmfb8JBqVmcC c2_cell_0;
+
+#endif                                 /* typedef_c2_cell_0 */
+
+#ifndef typedef_c2_cell_wrap_2
+#define typedef_c2_cell_wrap_2
+
+typedef struct c2_tag_x1FQ0fE4IJ1IDz5pAzueIE c2_cell_wrap_2;
+
+#endif                                 /* typedef_c2_cell_wrap_2 */
+
+#ifndef typedef_c2_cell_wrap_3
+#define typedef_c2_cell_wrap_3
+
+typedef struct c2_tag_hfuMxtM3511pO8GfZjii3B c2_cell_wrap_3;
+
+#endif                                 /* typedef_c2_cell_wrap_3 */
+
+#ifndef typedef_c2_cell_wrap_4
+#define typedef_c2_cell_wrap_4
+
+typedef struct c2_tag_fisVJns06LoJfzAcBfz34B c2_cell_wrap_4;
+
+#endif                                 /* typedef_c2_cell_wrap_4 */
+
+#ifndef typedef_c2_cell_5
+#define typedef_c2_cell_5
+
+typedef struct c2_tag_njgfiHhWBCqqqpWsKZxr7F c2_cell_5;
+
+#endif                                 /* typedef_c2_cell_5 */
+
+#ifndef typedef_c2_s_FDrX8kOEjZXLXru8nW4swE
+#define typedef_c2_s_FDrX8kOEjZXLXru8nW4swE
+
+typedef struct c2_tag_FDrX8kOEjZXLXru8nW4swE c2_s_FDrX8kOEjZXLXru8nW4swE;
+
+#endif                                 /* typedef_c2_s_FDrX8kOEjZXLXru8nW4swE */
+
+#ifndef typedef_c2_s_tiFQuw0ZDonsvxQULxkHSF
+#define typedef_c2_s_tiFQuw0ZDonsvxQULxkHSF
+
+typedef struct c2_tag_tiFQuw0ZDonsvxQULxkHSF c2_s_tiFQuw0ZDonsvxQULxkHSF;
+
+#endif                                 /* typedef_c2_s_tiFQuw0ZDonsvxQULxkHSF */
+
+#ifndef typedef_c2_s_tbVn9dOCpHC4rdIH56P9a
+#define typedef_c2_s_tbVn9dOCpHC4rdIH56P9a
+
+typedef struct c2_tag_tbVn9dOCpHC4rdIH56P9a c2_s_tbVn9dOCpHC4rdIH56P9a;
+
+#endif                                 /* typedef_c2_s_tbVn9dOCpHC4rdIH56P9a */
+
+#ifndef typedef_c2_s_sjYWA9KelG1lgIW9XihuxC
+#define typedef_c2_s_sjYWA9KelG1lgIW9XihuxC
+
+typedef struct c2_tag_sjYWA9KelG1lgIW9XihuxC c2_s_sjYWA9KelG1lgIW9XihuxC;
+
+#endif                                 /* typedef_c2_s_sjYWA9KelG1lgIW9XihuxC */
+
+#ifndef typedef_c2_s_r8oingdPeAlKT8PJg7lrLB
+#define typedef_c2_s_r8oingdPeAlKT8PJg7lrLB
+
+typedef struct c2_tag_r8oingdPeAlKT8PJg7lrLB c2_s_r8oingdPeAlKT8PJg7lrLB;
+
+#endif                                 /* typedef_c2_s_r8oingdPeAlKT8PJg7lrLB */
+
+#ifndef typedef_c2_s_zUYCxtzNwHeXvBwj4o5lLE
+#define typedef_c2_s_zUYCxtzNwHeXvBwj4o5lLE
+
+typedef struct c2_tag_zUYCxtzNwHeXvBwj4o5lLE c2_s_zUYCxtzNwHeXvBwj4o5lLE;
+
+#endif                                 /* typedef_c2_s_zUYCxtzNwHeXvBwj4o5lLE */
+
+#ifndef typedef_c2_s_uzuPWHtc1cM7ZRTfbsKeiF
+#define typedef_c2_s_uzuPWHtc1cM7ZRTfbsKeiF
+
+typedef struct c2_tag_uzuPWHtc1cM7ZRTfbsKeiF c2_s_uzuPWHtc1cM7ZRTfbsKeiF;
+
+#endif                                 /* typedef_c2_s_uzuPWHtc1cM7ZRTfbsKeiF */
+
+#ifndef typedef_c2_s_w3m1Q26ivrDTAtgc0mcqVE
+#define typedef_c2_s_w3m1Q26ivrDTAtgc0mcqVE
+
+typedef struct c2_tag_w3m1Q26ivrDTAtgc0mcqVE c2_s_w3m1Q26ivrDTAtgc0mcqVE;
+
+#endif                                 /* typedef_c2_s_w3m1Q26ivrDTAtgc0mcqVE */
+
+/* Type Definitions */
+#ifndef struct_c2_tag_s7m1Hk28rRB73vIGeeW9J6G
+#define struct_c2_tag_s7m1Hk28rRB73vIGeeW9J6G
+
+struct c2_tag_s7m1Hk28rRB73vIGeeW9J6G
+{
+  uint32_T provider;
+  uint32_T DataFormat;
+};
+
+#endif                                 /* struct_c2_tag_s7m1Hk28rRB73vIGeeW9J6G */
+
+#ifndef typedef_c2_s7m1Hk28rRB73vIGeeW9J6G
+#define typedef_c2_s7m1Hk28rRB73vIGeeW9J6G
+
+typedef struct c2_tag_s7m1Hk28rRB73vIGeeW9J6G c2_s7m1Hk28rRB73vIGeeW9J6G;
+
+#endif                                 /* typedef_c2_s7m1Hk28rRB73vIGeeW9J6G */
+
+#ifndef struct_c2_tag_sa1bWj2vFQaBkpnMZWMpj5
+#define struct_c2_tag_sa1bWj2vFQaBkpnMZWMpj5
+
+struct c2_tag_sa1bWj2vFQaBkpnMZWMpj5
+{
+  uint32_T Sec;
+  uint32_T Nsec;
+};
+
+#endif                                 /* struct_c2_tag_sa1bWj2vFQaBkpnMZWMpj5 */
+
+#ifndef typedef_c2_ros_TimeStruct_T
+#define typedef_c2_ros_TimeStruct_T
+
+typedef struct c2_tag_sa1bWj2vFQaBkpnMZWMpj5 c2_ros_TimeStruct_T;
+
+#endif                                 /* typedef_c2_ros_TimeStruct_T */
+
+#ifndef struct_c2_tag_smzGQHcQ1fZcSCW5rtLpn4F
+#define struct_c2_tag_smzGQHcQ1fZcSCW5rtLpn4F
+
+struct c2_tag_smzGQHcQ1fZcSCW5rtLpn4F
+{
+  boolean_T CaseSensitivity;
+  char_T PartialMatching[6];
+  boolean_T StructExpand;
+  boolean_T IgnoreNulls;
+  boolean_T SupportOverrides;
+};
+
+#endif                                 /* struct_c2_tag_smzGQHcQ1fZcSCW5rtLpn4F */
+
+#ifndef typedef_c2_smzGQHcQ1fZcSCW5rtLpn4F
+#define typedef_c2_smzGQHcQ1fZcSCW5rtLpn4F
+
+typedef struct c2_tag_smzGQHcQ1fZcSCW5rtLpn4F c2_smzGQHcQ1fZcSCW5rtLpn4F;
+
+#endif                                 /* typedef_c2_smzGQHcQ1fZcSCW5rtLpn4F */
+
+#ifndef struct_c2_tag_sdSMOHVTauEd1rUkHy24MYH
+#define struct_c2_tag_sdSMOHVTauEd1rUkHy24MYH
+
+struct c2_tag_sdSMOHVTauEd1rUkHy24MYH
+{
+  char_T HeaderFile[15];
+  char_T PubSubHeaderFile[22];
+  char_T ParamHeaderFile[21];
+  char_T ServiceHeaderFile[23];
+  char_T ImageHeaderFile[18];
+  char_T TimeHeaderFile[12];
+  char_T ParamSourceFile[23];
+  char_T ConversionUtilsHeaderFile[24];
+  char_T OverrunDetectionSourceFile[24];
+  char_T Location[58];
+};
+
+#endif                                 /* struct_c2_tag_sdSMOHVTauEd1rUkHy24MYH */
+
+#ifndef typedef_c2_sdSMOHVTauEd1rUkHy24MYH
+#define typedef_c2_sdSMOHVTauEd1rUkHy24MYH
+
+typedef struct c2_tag_sdSMOHVTauEd1rUkHy24MYH c2_sdSMOHVTauEd1rUkHy24MYH;
+
+#endif                                 /* typedef_c2_sdSMOHVTauEd1rUkHy24MYH */
+
+#ifndef struct_c2_tag_bhoJb8DGvrqWQvPGAcJgFD
+#define struct_c2_tag_bhoJb8DGvrqWQvPGAcJgFD
+
+struct c2_tag_bhoJb8DGvrqWQvPGAcJgFD
+{
+  char_T Value[10];
+};
+
+#endif                                 /* struct_c2_tag_bhoJb8DGvrqWQvPGAcJgFD */
+
+#ifndef typedef_c2_s_bhoJb8DGvrqWQvPGAcJgFD
+#define typedef_c2_s_bhoJb8DGvrqWQvPGAcJgFD
+
+typedef struct c2_tag_bhoJb8DGvrqWQvPGAcJgFD c2_s_bhoJb8DGvrqWQvPGAcJgFD;
+
+#endif                                 /* typedef_c2_s_bhoJb8DGvrqWQvPGAcJgFD */
+
+#ifndef struct_c2_tag_0U5Qpi6GXqcmfb8JBqVmcC
+#define struct_c2_tag_0U5Qpi6GXqcmfb8JBqVmcC
+
+struct c2_tag_0U5Qpi6GXqcmfb8JBqVmcC
+{
+  char_T f1[7];
+  char_T f2[4];
+  char_T f3[6];
+};
+
+#endif                                 /* struct_c2_tag_0U5Qpi6GXqcmfb8JBqVmcC */
+
+#ifndef typedef_c2_cell_0
+#define typedef_c2_cell_0
+
+typedef struct c2_tag_0U5Qpi6GXqcmfb8JBqVmcC c2_cell_0;
+
+#endif                                 /* typedef_c2_cell_0 */
+
+#ifndef struct_c2_tag_x1FQ0fE4IJ1IDz5pAzueIE
+#define struct_c2_tag_x1FQ0fE4IJ1IDz5pAzueIE
+
+struct c2_tag_x1FQ0fE4IJ1IDz5pAzueIE
+{
+  char_T f1[3];
+};
+
+#endif                                 /* struct_c2_tag_x1FQ0fE4IJ1IDz5pAzueIE */
+
+#ifndef typedef_c2_cell_wrap_2
+#define typedef_c2_cell_wrap_2
+
+typedef struct c2_tag_x1FQ0fE4IJ1IDz5pAzueIE c2_cell_wrap_2;
+
+#endif                                 /* typedef_c2_cell_wrap_2 */
+
+#ifndef struct_c2_tag_hfuMxtM3511pO8GfZjii3B
+#define struct_c2_tag_hfuMxtM3511pO8GfZjii3B
+
+struct c2_tag_hfuMxtM3511pO8GfZjii3B
+{
+  char_T f1[8];
+};
+
+#endif                                 /* struct_c2_tag_hfuMxtM3511pO8GfZjii3B */
+
+#ifndef typedef_c2_cell_wrap_3
+#define typedef_c2_cell_wrap_3
+
+typedef struct c2_tag_hfuMxtM3511pO8GfZjii3B c2_cell_wrap_3;
+
+#endif                                 /* typedef_c2_cell_wrap_3 */
+
+#ifndef struct_c2_tag_fisVJns06LoJfzAcBfz34B
+#define struct_c2_tag_fisVJns06LoJfzAcBfz34B
+
+struct c2_tag_fisVJns06LoJfzAcBfz34B
+{
+  char_T f1[10];
+};
+
+#endif                                 /* struct_c2_tag_fisVJns06LoJfzAcBfz34B */
+
+#ifndef typedef_c2_cell_wrap_4
+#define typedef_c2_cell_wrap_4
+
+typedef struct c2_tag_fisVJns06LoJfzAcBfz34B c2_cell_wrap_4;
+
+#endif                                 /* typedef_c2_cell_wrap_4 */
+
+#ifndef struct_c2_tag_njgfiHhWBCqqqpWsKZxr7F
+#define struct_c2_tag_njgfiHhWBCqqqpWsKZxr7F
+
+struct c2_tag_njgfiHhWBCqqqpWsKZxr7F
+{
+  char_T f1[15];
+  char_T f2[15];
+  char_T f3[12];
+  char_T f4[11];
+  char_T f5[16];
+};
+
+#endif                                 /* struct_c2_tag_njgfiHhWBCqqqpWsKZxr7F */
+
+#ifndef typedef_c2_cell_5
+#define typedef_c2_cell_5
+
+typedef struct c2_tag_njgfiHhWBCqqqpWsKZxr7F c2_cell_5;
+
+#endif                                 /* typedef_c2_cell_5 */
+
+#ifndef struct_c2_tag_FDrX8kOEjZXLXru8nW4swE
+#define struct_c2_tag_FDrX8kOEjZXLXru8nW4swE
+
+struct c2_tag_FDrX8kOEjZXLXru8nW4swE
+{
+  char_T f1[6];
+  char_T f2[6];
+};
+
+#endif                                 /* struct_c2_tag_FDrX8kOEjZXLXru8nW4swE */
+
+#ifndef typedef_c2_s_FDrX8kOEjZXLXru8nW4swE
+#define typedef_c2_s_FDrX8kOEjZXLXru8nW4swE
+
+typedef struct c2_tag_FDrX8kOEjZXLXru8nW4swE c2_s_FDrX8kOEjZXLXru8nW4swE;
+
+#endif                                 /* typedef_c2_s_FDrX8kOEjZXLXru8nW4swE */
+
+#ifndef struct_c2_tag_tiFQuw0ZDonsvxQULxkHSF
+#define struct_c2_tag_tiFQuw0ZDonsvxQULxkHSF
+
+struct c2_tag_tiFQuw0ZDonsvxQULxkHSF
+{
+  char_T Value[5];
+};
+
+#endif                                 /* struct_c2_tag_tiFQuw0ZDonsvxQULxkHSF */
+
+#ifndef typedef_c2_s_tiFQuw0ZDonsvxQULxkHSF
+#define typedef_c2_s_tiFQuw0ZDonsvxQULxkHSF
+
+typedef struct c2_tag_tiFQuw0ZDonsvxQULxkHSF c2_s_tiFQuw0ZDonsvxQULxkHSF;
+
+#endif                                 /* typedef_c2_s_tiFQuw0ZDonsvxQULxkHSF */
+
+#ifndef struct_c2_tag_tbVn9dOCpHC4rdIH56P9a
+#define struct_c2_tag_tbVn9dOCpHC4rdIH56P9a
+
+struct c2_tag_tbVn9dOCpHC4rdIH56P9a
+{
+  c2_cell_0 _data;
+};
+
+#endif                                 /* struct_c2_tag_tbVn9dOCpHC4rdIH56P9a */
+
+#ifndef typedef_c2_s_tbVn9dOCpHC4rdIH56P9a
+#define typedef_c2_s_tbVn9dOCpHC4rdIH56P9a
+
+typedef struct c2_tag_tbVn9dOCpHC4rdIH56P9a c2_s_tbVn9dOCpHC4rdIH56P9a;
+
+#endif                                 /* typedef_c2_s_tbVn9dOCpHC4rdIH56P9a */
+
+#ifndef struct_c2_tag_sjYWA9KelG1lgIW9XihuxC
+#define struct_c2_tag_sjYWA9KelG1lgIW9XihuxC
+
+struct c2_tag_sjYWA9KelG1lgIW9XihuxC
+{
+  c2_cell_wrap_2 _data;
+};
+
+#endif                                 /* struct_c2_tag_sjYWA9KelG1lgIW9XihuxC */
+
+#ifndef typedef_c2_s_sjYWA9KelG1lgIW9XihuxC
+#define typedef_c2_s_sjYWA9KelG1lgIW9XihuxC
+
+typedef struct c2_tag_sjYWA9KelG1lgIW9XihuxC c2_s_sjYWA9KelG1lgIW9XihuxC;
+
+#endif                                 /* typedef_c2_s_sjYWA9KelG1lgIW9XihuxC */
+
+#ifndef struct_c2_tag_r8oingdPeAlKT8PJg7lrLB
+#define struct_c2_tag_r8oingdPeAlKT8PJg7lrLB
+
+struct c2_tag_r8oingdPeAlKT8PJg7lrLB
+{
+  c2_cell_wrap_3 _data;
+};
+
+#endif                                 /* struct_c2_tag_r8oingdPeAlKT8PJg7lrLB */
+
+#ifndef typedef_c2_s_r8oingdPeAlKT8PJg7lrLB
+#define typedef_c2_s_r8oingdPeAlKT8PJg7lrLB
+
+typedef struct c2_tag_r8oingdPeAlKT8PJg7lrLB c2_s_r8oingdPeAlKT8PJg7lrLB;
+
+#endif                                 /* typedef_c2_s_r8oingdPeAlKT8PJg7lrLB */
+
+#ifndef struct_c2_tag_zUYCxtzNwHeXvBwj4o5lLE
+#define struct_c2_tag_zUYCxtzNwHeXvBwj4o5lLE
+
+struct c2_tag_zUYCxtzNwHeXvBwj4o5lLE
+{
+  c2_cell_wrap_4 _data;
+};
+
+#endif                                 /* struct_c2_tag_zUYCxtzNwHeXvBwj4o5lLE */
+
+#ifndef typedef_c2_s_zUYCxtzNwHeXvBwj4o5lLE
+#define typedef_c2_s_zUYCxtzNwHeXvBwj4o5lLE
+
+typedef struct c2_tag_zUYCxtzNwHeXvBwj4o5lLE c2_s_zUYCxtzNwHeXvBwj4o5lLE;
+
+#endif                                 /* typedef_c2_s_zUYCxtzNwHeXvBwj4o5lLE */
+
+#ifndef struct_c2_tag_uzuPWHtc1cM7ZRTfbsKeiF
+#define struct_c2_tag_uzuPWHtc1cM7ZRTfbsKeiF
+
+struct c2_tag_uzuPWHtc1cM7ZRTfbsKeiF
+{
+  c2_cell_5 _data;
+};
+
+#endif                                 /* struct_c2_tag_uzuPWHtc1cM7ZRTfbsKeiF */
+
+#ifndef typedef_c2_s_uzuPWHtc1cM7ZRTfbsKeiF
+#define typedef_c2_s_uzuPWHtc1cM7ZRTfbsKeiF
+
+typedef struct c2_tag_uzuPWHtc1cM7ZRTfbsKeiF c2_s_uzuPWHtc1cM7ZRTfbsKeiF;
+
+#endif                                 /* typedef_c2_s_uzuPWHtc1cM7ZRTfbsKeiF */
+
+#ifndef struct_c2_tag_w3m1Q26ivrDTAtgc0mcqVE
+#define struct_c2_tag_w3m1Q26ivrDTAtgc0mcqVE
+
+struct c2_tag_w3m1Q26ivrDTAtgc0mcqVE
+{
+  c2_s_FDrX8kOEjZXLXru8nW4swE _data;
+};
+
+#endif                                 /* struct_c2_tag_w3m1Q26ivrDTAtgc0mcqVE */
+
+#ifndef typedef_c2_s_w3m1Q26ivrDTAtgc0mcqVE
+#define typedef_c2_s_w3m1Q26ivrDTAtgc0mcqVE
+
+typedef struct c2_tag_w3m1Q26ivrDTAtgc0mcqVE c2_s_w3m1Q26ivrDTAtgc0mcqVE;
+
+#endif                                 /* typedef_c2_s_w3m1Q26ivrDTAtgc0mcqVE */
+
+#ifndef typedef_SFc2_simulationInstanceStruct
+#define typedef_SFc2_simulationInstanceStruct
+
+typedef struct {
+  SimStruct *S;
+  ChartInfoStruct chartInfo;
+  int32_T c2_sfEvent;
+  boolean_T c2_doneDoubleBufferReInit;
+  uint8_T c2_is_active_c2_simulation;
+  uint8_T c2_JITStateAnimation[1];
+  uint8_T c2_JITTransitionAnimation[1];
+  void *c2_RuntimeVar;
+  int32_T c2_IsDebuggerActive;
+  int32_T c2_IsSequenceViewerPresent;
+  int32_T c2_SequenceViewerOptimization;
+  int32_T c2_IsHeatMapPresent;
+  int32_T c2_IsStmtByStmtMode;
+  uint32_T c2_mlFcnLineNumber;
+  uint32_T c2_numFcnVars;
+  CovrtStateflowInstance *c2_covrtInstance;
+  void *c2_fEmlrtCtx;
+  uint32_T *c2_Sec;
+  uint32_T *c2_Nsec;
+} SFc2_simulationInstanceStruct;
+
+#endif                                 /* typedef_SFc2_simulationInstanceStruct */
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+
+/* Function Declarations */
+extern const mxArray *sf_c2_simulation_get_eml_resolved_functions_info(void);
+
+/* Function Definitions */
+extern void sf_c2_simulation_get_check_sum(mxArray *plhs[]);
+extern void c2_simulation_method_dispatcher(SimStruct *S, int_T method, void
+  *data);
+
+#endif
